92cf70d2a19f669635f033d5276d480d3e8e65bb
MiguelSerralde <85590150+MiguelSerralde@users.noreply.github.com>
2023-03-14 16:57:02 -0700

    BlurEffect

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function max_imgData(){    
+    document.getElementById("menu_left").style.filter = "blur(2px)"
+    document.getElementById("menu_right").style.filter = "blur(2px)"
+    document.getElementById("logo").style.filter = "blur(2px)"   
+    document.getElementById("home_menu").style.pointerEvents = "none"
+    document.getElementById("news_menu").style.pointerEvents = "none"
+    document.getElementById("trivia_menu").style.pointerEvents = "none"
+    document.getElementById("about_menu").style.pointerEvents = "none"
+    document.getElementById("middle").style.filter = "blur(2px)"
+}
